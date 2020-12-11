3529aea6b83ea6462064f01135c168e9b2847dcd
Pey-crypto <53893419+Pey-crypto@users.noreply.github.com>
2020-12-11 21:42:39 +0530

    Lets make it more accessible

--- project-void/src/js/lib/media.js
+++ project-void/src/js/lib/media.js
@@ -8,11 +8,9 @@ export default async function getMyStream() {
     facingMode: "user",
     width: {min: 640, ideal: 1280, max: 1920},
     height: {min: 360, ideal: 720, max: 1080},
-    frameRate: {ideal: 15, max: 24},
   }
   const audio = {
     autoGainControl: true,
-    sampleRate: {ideal: 48000, min: 35000},
     echoCancellation: true,
     channelCount: {ideal: 1},
   }
