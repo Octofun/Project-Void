c05648ade474e7d15717458c5c862590c0b058cd
Krishnapriya S <kpriya102000@gmail.com>
2020-12-19 14:41:03 +0530

    changes in footer and removed conta.js

--- project-void/src/js/components/footer.js
+++ project-void/src/js/components/footer.js
@@ -9,7 +9,7 @@ export default (props) => {
       <div className='container'>
       <p>Void is a free and open source project, still under development. For source code go to <a href="https://github.com/Octofun/Project-Void.git">Project VOID</a>.</p>
         <div className='row'>
-          <a href='/contact'>Contact Us</a>
+          <a href="mailto:kpriya102000@gmail.com,npeyara@gmail.com?subject=Message from website visitor">Contact Us</a>
 
         </div>
         <div className='row'>
